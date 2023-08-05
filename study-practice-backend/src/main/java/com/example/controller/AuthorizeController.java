ad641432a0247ed4fc597a4c853a03c0ab7044de
Joey <headmastereggy@gmail.com>
2023-08-05 12:21:14 +0800

    changes

--- study-practice-backend/src/main/java/com/example/controller/AuthorizeController.java
+++ study-practice-backend/src/main/java/com/example/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import com.example.service.AuthorizeService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,16 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthorizeController {
 
+    private final String USERNAME_REGEX = "^[a-zA-Z0-9一-龥]+$";
     private final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$";
     @Resource
     AuthorizeService authorizeService;
 
     @PostMapping("/valid-register-email")
     public RestBean<String> validateEmail(@Pattern(regexp = EMAIL_REGEX) @RequestParam("email") String email, HttpSession session) {
-        if (authorizeService.sendValidateEmail(email, session.getId())) {
+        String s = authorizeService.sendValidateEmail(email, session.getId());
+        if (s == null)
             return RestBean.success("邮件已发送，请注意查收");
-        } else {
-            return RestBean.failure(400, "邮件发送失败，请联系管理员");
-        }
+        else
+            return RestBean.failure(400, s);
+    }
+
+    @PostMapping("/register")
+    public RestBean<String> registerUser(@Pattern(regexp = USERNAME_REGEX) @Length(min = 2, max = 8) @RequestParam("username") String username,
+                                         @Length(min = 6, max = 16) @RequestParam("password") String password,
+                                         @RequestParam("email") String email,
+                                         @Length(min = 6, max = 6) @RequestParam("code") String code,
+                                         HttpSession session) {
+        String s = authorizeService.validateAndRegister(username, password, email, code, session.getId());
+        if (s == null)
+            return RestBean.success("注册成功");
+        else
+            return RestBean.failure(400, s);
+
     }
 }
--- study-practice-backend/src/main/java/com/example/service/impl/AuthorizeServiceImpl.java
+++ study-practice-backend/src/main/java/com/example/service/impl/AuthorizeServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -33,6 +34,8 @@ public class AuthorizeServiceImpl implements AuthorizeService {
     @Resource
     MailSender mailSender;
 
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         if (username == null) {
@@ -49,13 +52,38 @@ public class AuthorizeServiceImpl implements AuthorizeService {
     }
 
     @Override
-    public boolean sendValidateEmail(String email, String sessionId) {
+    public String validateAndRegister(String username, String password, String email, String code, String sessionId) {
+        String key = "email: " + sessionId + ": " + email;
+        if (Boolean.TRUE.equals(stringRedisTemplate.hasKey(key))) {
+            String s = stringRedisTemplate.opsForValue().get(key);
+            if (s == null) {
+                return "验证码已失效，请重新获取验证码";
+            } else if (s.equals(code)) {
+                password = bCryptPasswordEncoder.encode(password);
+                if(mapper.createAccount(username, password,email) > 0) {
+                    return null;
+                }else {
+                    return "内部错误，请联系管理员";
+                }
+            } else {
+                return "验证码错误，请核对后重新输入";
+            }
+        } else {
+            return "请先获取验证码";
+        }
+    }
+
+    @Override
+    public String sendValidateEmail(String email, String sessionId) {
         String key = "email: " + sessionId + ": " + email;
         if (Boolean.TRUE.equals(stringRedisTemplate.hasKey(key))) {
             Long expire = Optional.ofNullable(stringRedisTemplate.getExpire(key, TimeUnit.SECONDS)).orElse(0L);
             if (expire > 120) {
-                return false;
+                return "请求频繁，请稍后重试";
             }
+        }
+        if (mapper.findAccountByNameOrEmail(email) != null) {
+            return "此邮箱已被注册";
         }
         Random random = new Random();
         int code = random.nextInt(900000) + 100000;
@@ -67,10 +95,12 @@ public class AuthorizeServiceImpl implements AuthorizeService {
         try {
             mailSender.send(simpleMailMessage);
             stringRedisTemplate.opsForValue().set(key, String.valueOf(code), 3, TimeUnit.MINUTES);
-            return true;
+            return null;
         } catch (MailException e) {
             e.printStackTrace();
-            return false;
+            return "邮件发送失败，请检查邮件地址是否有效";
         }
+
+
     }
 }
