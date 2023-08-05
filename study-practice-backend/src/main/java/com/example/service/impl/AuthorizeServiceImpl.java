585e3f7fbdf57f552f1eb3346f0bef87180aeda4
Joey <headmastereggy@gmail.com>
2023-08-05 22:58:28 +0800

    changes

--- study-practice-backend/src/main/java/com/example/service/impl/AuthorizeServiceImpl.java
+++ study-practice-backend/src/main/java/com/example/service/impl/AuthorizeServiceImpl.java
@@ -26,25 +26,23 @@ public class AuthorizeServiceImpl implements AuthorizeService {
     String from;
 
     @Resource
-    StringRedisTemplate stringRedisTemplate;
-
-    @Resource
     UserMapper mapper;
 
     @Resource
     MailSender mailSender;
 
-    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    @Resource
+    StringRedisTemplate template;
+
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (username == null) {
+        if (username == null)
             throw new UsernameNotFoundException("用户名不能为空");
-        }
         Account account = mapper.findAccountByNameOrEmail(username);
-        if (account == null) {
+        if (account == null)
             throw new UsernameNotFoundException("用户名或密码错误");
-        }
         return User
                 .withUsername(account.getUsername())
                 .password(account.getPassword())
@@ -53,38 +51,64 @@ public class AuthorizeServiceImpl implements AuthorizeService {
     }
 
     @Override
+    public String sendValidateEmail(String email, String sessionId, boolean hasAccount) {
+        String key = "email:" + sessionId + ":" + email + ":" + hasAccount;
+        if (Boolean.TRUE.equals(template.hasKey(key))) {
+            Long expire = Optional.ofNullable(template.getExpire(key, TimeUnit.SECONDS)).orElse(0L);
+            if (expire > 120) return "请求频繁，请稍后再试";
+        }
+        Account account = mapper.findAccountByNameOrEmail(email);
+        if (hasAccount && account == null) return "没有此邮件地址的账户";
+        if (!hasAccount && account != null) return "此邮箱已被其他用户注册";
+        Random random = new Random();
+        int code = random.nextInt(899999) + 100000;
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(email);
+        message.setSubject("您的验证邮件");
+        message.setText("验证码是：" + code);
+        try {
+            mailSender.send(message);
+            template.opsForValue().set(key, String.valueOf(code), 3, TimeUnit.MINUTES);
+            return null;
+        } catch (MailException e) {
+            e.printStackTrace();
+            return "邮件发送失败，请坚持邮件地址是否有效";
+        }
+    }
+
+    @Override
     public String validateAndRegister(String username, String password, String email, String code, String sessionId) {
-        String key = "email: " + sessionId + ": " + email + ":false";
-        if (Boolean.TRUE.equals(stringRedisTemplate.hasKey(key))) {
-            String s = stringRedisTemplate.opsForValue().get(key);
-            if (s == null) {
-                return "验证码已失效，请重新获取验证码";
-            } else if (s.equals(code)) {
+        String key = "email:" + sessionId + ":" + email + ":false";
+        if (Boolean.TRUE.equals(template.hasKey(key))) {
+            String s = template.opsForValue().get(key);
+            if (s == null) return "验证码失效，请重新请求";
+            if (s.equals(code)) {
                 Account account = mapper.findAccountByNameOrEmail(username);
                 if (account != null) return "此用户名已被注册，请更换用户名";
-                password = bCryptPasswordEncoder.encode(password);
-                stringRedisTemplate.delete(key);
+                template.delete(key);
+                password = encoder.encode(password);
                 if (mapper.createAccount(username, password, email) > 0) {
                     return null;
                 } else {
                     return "内部错误，请联系管理员";
                 }
             } else {
-                return "验证码错误，请核对后重新输入";
+                return "验证码错误，请检查后再提交";
             }
         } else {
-            return "请先获取验证码";
+            return "请先请求一封验证码邮件";
         }
     }
 
     @Override
     public String validateOnly(String email, String code, String sessionId) {
         String key = "email:" + sessionId + ":" + email + ":true";
-        if (Boolean.TRUE.equals(stringRedisTemplate.hasKey(key))) {
-            String s = stringRedisTemplate.opsForValue().get(key);
+        if (Boolean.TRUE.equals(template.hasKey(key))) {
+            String s = template.opsForValue().get(key);
             if (s == null) return "验证码失效，请重新请求";
             if (s.equals(code)) {
-                stringRedisTemplate.delete(key);
+                template.delete(key);
                 return null;
             } else {
                 return "验证码错误，请检查后再提交";
@@ -96,40 +120,7 @@ public class AuthorizeServiceImpl implements AuthorizeService {
 
     @Override
     public boolean resetPassword(String password, String email) {
-        password = bCryptPasswordEncoder.encode(password);
+        password = encoder.encode(password);
         return mapper.resetPasswordByEmail(password, email) > 0;
-    }
-
-    @Override
-    public String sendValidateEmail(String email, String sessionId, boolean hasAccount) {
-        String key = "email: " + sessionId + ": " + email + ": " + hasAccount;
-        if (Boolean.TRUE.equals(stringRedisTemplate.hasKey(key))) {
-            Long expire = Optional.ofNullable(stringRedisTemplate.getExpire(key, TimeUnit.SECONDS)).orElse(0L);
-            if (expire > 120) {
-                return "请求频繁，请稍后重试";
-            }
-        }
-        Account account = mapper.findAccountByNameOrEmail(email);
-        if (hasAccount && account == null) {
-            return "没有此邮件地址的账户";
-        }
-        if (!hasAccount && account != null) {
-            return "此邮箱已被注册";
-        }
-        Random random = new Random();
-        int code = random.nextInt(900000) + 100000;
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom(from);
-        simpleMailMessage.setTo(email);
-        simpleMailMessage.setSubject("您的验证邮件");
-        simpleMailMessage.setText("验证码是: " + code);
-        try {
-            mailSender.send(simpleMailMessage);
-            stringRedisTemplate.opsForValue().set(key, String.valueOf(code), 3, TimeUnit.MINUTES);
-            return null;
-        } catch (MailException e) {
-            e.printStackTrace();
-            return "邮件发送失败，请检查邮件地址是否有效";
-        }
     }
 }
